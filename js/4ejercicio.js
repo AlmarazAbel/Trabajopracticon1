688f5ed5dbd55e24349e85bfe2a99da533291761
Abel Almaraz <almarazabeljesus@gmail.com>
2026-03-09 15:38:36 -0300

    resolucion ejercicio 3

---
+++ js/4ejercicio.js
@@ -0,0 +1,5 @@
+//4.- Escribe un programa de dos líneas que pida el nombre del usuario con un
+//  prompt y escriba un texto que diga «Hola nombreUsuario»
+
+let nombre = prompt('ingrese un nombre :')
+document.write(`hola ${nombre} ` )
